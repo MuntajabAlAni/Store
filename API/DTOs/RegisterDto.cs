676d078353b42dc9c5a5193632ef130fe4153d34
Muntajab Aldeen <muntajab_alani@yahoo.com>
2022-06-11 16:09:11 +0300

    no swagger config ! done validation

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,8 +1,11 @@
-﻿namespace API.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
 
 public class RegisterDto
 {
-    public string DisplayName { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
+   [Required] public string DisplayName { get; set; }
+   [Required] [EmailAddress] public string Email { get; set; }
+   [Required] [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", ErrorMessage = "Password Requirements wrong ! 1 1 1 1 ")] 
+   public string Password { get; set; }
 }
